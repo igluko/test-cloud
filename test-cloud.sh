652d41e2b27db279cde20a49365da2eb5e4b825b
igluko <igluko@gmail.com>
2022-01-09 22:41:20 +0300

    replace ">>" to "| tee -a"

--- test-cloud.sh
+++ test-cloud.sh
@@ -6,11 +6,23 @@ if [ "$EUID" -ne 0 ]; then echo "Error: non root user, use root"; exit 1; fi
 printf "MemTotal: " | tee -a result.yml
 grep MemTotal /proc/meminfo | awk '{print $2}' | tee -a result.yml
 #Объем swap
-printf "SwapTotal: " >> result.yml
-grep SwapTotal /proc/meminfo | awk '{print $2}' >> result.yml
+printf "SwapTotal: " | tee -a result.yml
+grep SwapTotal /proc/meminfo | awk '{print $2}' | tee -a result.yml
 #Объем занятого места
-printf "usedSpace: " >> result.yml
-df | awk '$6=="/" {print $3}' >> result.yml
+printf "usedSpace: " | tee -a result.yml
+df | awk '$6=="/" {print $3}' | tee -a result.yml
+#Общее количество пакетов
+printf "packetsCount: " | tee -a result.yml
+dpkg -l | grep -c '^ii' | tee -a result.yml
+#Время обновление дерева пакетов
+printf "timeUpdateTree: " | tee -a result.yml
+(time apt update 1>&0) 2>&1 | awk '$1=="real" {print $2}' | tee -a result.yml
+#Количество и размер обновлений
+printf "sizeUpgrade: " | tee -a result.yml
+apt dist-upgrade --assume-no | grep -P 'After this' | grep -oP '\d.*B' | tee -a result.yml
+#Время обновления установленных пакетов
+printf "timeUpgrade: " | tee -a result.yml
+(time DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y 1>&0) 2>&1 | awk '$1=="real" {print $2}' | tee -a result.yml
 #CPU benchmark
 #-r [1-3]: [CPU only, Memory only, All tests] Autorun tests and export scores to [results_cpu.yml, results_memory.yml, results_all.yml]
 curl https://www.passmark.com/downloads/pt_linux_x64.zip -o pt_linux_x64.zip
@@ -18,32 +30,20 @@ apt install unzip
 unzip pt_linux_x64.zip -y
 apt install libncurses5 -y
 PerformanceTest/pt_linux_x64 -r 3
-cat results_all.yml | grep OSName >> result.yml
-cat results_all.yml | grep Kernel >> result.yml
-cat results_all.yml | grep Processor >> result.yml
-cat results_all.yml | grep NumLogicals >> result.yml
-cat results_all.yml | grep Memory >> result.yml
-cat results_all.yml | grep NumLogicals >> result.yml
-cat results_all.yml | grep SUMM_CPU >> result.yml
-cat results_all.yml | grep SUMM_ME >> result.yml
-#Общее количество пакетов
-printf "packetsCount: " >> result.yml
-dpkg -l | grep -c '^ii' >> result.yml
-#Время обновление дерева пакетов
-printf "timeUpdateTree: " >> result.yml
-(time apt update 1>&0) 2>&1 | awk '$1=="real" {print $2}' >> result.yml
-#Количество и размер обновлений
-printf "sizeUpgrade: " >> result.yml
-apt dist-upgrade --assume-no | grep -P 'After this' | grep -oP '\d.*B' >> result.yml
-#Время обновления установленных пакетов
-printf "timeUpgrade: " >> result.yml
-(time DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y 1>&0) 2>&1 | awk '$1=="real" {print $2}' >> result.yml
+cat results_all.yml | grep OSName | tee -a result.yml
+cat results_all.yml | grep Kernel | tee -a result.yml
+cat results_all.yml | grep Processor | tee -a result.yml
+cat results_all.yml | grep NumLogicals | tee -a result.yml
+cat results_all.yml | grep Memory | tee -a result.yml
+cat results_all.yml | grep NumLogicals | tee -a result.yml
+cat results_all.yml | grep SUMM_CPU | tee -a result.yml
+cat results_all.yml | grep SUMM_ME | tee -a result.yml
 #Время установки докера
-printf "timeInstallDocker: " >> result.yml
-(time apt install docker docker-compose -y 1>&0) 2>&1 | awk '$1=="real" {print $2}' >> result.yml
+printf "timeInstallDocker: " | tee -a result.yml
+(time apt install docker docker-compose -y 1>&0) 2>&1 | awk '$1=="real" {print $2}' | tee -a result.yml
 #Версия докера
-printf "versionDocker: " >> result.yml
-docker info |  awk -F ":" '$1==" Server Version" {print $2}' | xargs | xargs >> result.yml
+printf "versionDocker: " | tee -a result.yml
+docker info |  awk -F ":" '$1==" Server Version" {print $2}' | xargs | xargs | tee -a result.yml
 #Spedtest
 apt-get install curl
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
